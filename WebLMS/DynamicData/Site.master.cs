b6d6a2b8a6483b6d3b4f8481521b37d40e810ab2
Savva <cabehok@inbox.ru>
2016-11-07 16:59:22 +0300

    added auth

--- WebLMS/DynamicData/Site.master.cs
+++ WebLMS/DynamicData/Site.master.cs
@@ -1,11 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Web.DynamicData;
 using System.Web.UI.WebControls;
+using System;
+using System.Net;
 
 namespace WebApplication2
 {
     public partial class Site : System.Web.UI.MasterPage
     {
+        protected override void OnInit(EventArgs e)
+        {
+            /*if (!Page.User.Identity.IsAuthenticated)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                Response.End();
+            }
 
+            if (!Page.User.IsInRole("Admin"))
+            {
+                Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                Response.End();
+            }*/
+
+            base.OnInit(e);
+        }
     }
 }
